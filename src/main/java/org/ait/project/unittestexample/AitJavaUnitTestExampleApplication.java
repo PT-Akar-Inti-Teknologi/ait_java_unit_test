d2fe4b0a1087720b45ba4878d4d1bc9c5224c24b
[AIT] Arseno Harahap <arseno.harahap@akarinti.tech>
2022-09-21 16:06:31 +0700

    initial(unit test example): first commit
    - initial commit for this project

---
+++ src/main/java/org/ait/project/unittestexample/AitJavaUnitTestExampleApplication.java
@@ -0,0 +1,14 @@
+package org.ait.project.unittestexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class AitJavaUnitTestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AitJavaUnitTestExampleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/ait/project/unittestexample/shared/constant/ProfileConstants.java
@@ -0,0 +1,7 @@
+package org.ait.project.unittestexample.shared.constant;
+
+public final class ProfileConstants {
+
+	public final static String SPRING_PROFILE_DEVELOPMENT = "dev";
+	public final static String SPRING_PROFILE_TEST = "test";
+}
