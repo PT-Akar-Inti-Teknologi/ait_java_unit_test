e7ce51c80a56bfc4cda65872d1ab5b490ccd4e19
[AIT] Arseno Harahap <arseno.harahap@akarinti.tech>
2022-09-21 16:10:30 +0700

    test: initial test case
    - spring initialization test

---
+++ src/test/java/org/ait/project/unittestexample/AitJavaUnitTestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ait.project.unittestexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitJavaUnitTestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
